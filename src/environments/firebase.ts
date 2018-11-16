5e183a25e6e32ae36a787b694383b0a1582e89d3
Menencia <menencia@gmail.com>
2018-11-16 22:40:48 +0100

    add firebase hosting

---
+++ src/environments/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDFlxTfNNBh4utzi2GH3CbejPn5xspXx6A',
+  authDomain: 'ffbe-progress.firebaseapp.com',
+  databaseURL: 'https://ffbe-progress.firebaseio.com',
+  projectId: 'ffbe-progress',
+  storageBucket: 'ffbe-progress.appspot.com',
+  messagingSenderId: '943397081240'
+};
